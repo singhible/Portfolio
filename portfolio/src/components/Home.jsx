295a63981149a66b67768256bb7fd075ea2e9ad9
singhible <bbnpreetsingh@gmail.com>
2022-11-06 13:57:21 -0800

    Built the ABout apge

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -10,8 +10,8 @@ const Home = () => {
             <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm an aspiring Full Stack developer.</h2>
             <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm also an university student who is currently building its web-based MacOS. </p>
             <div>
-                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
-                <span className='hover:rotate-90 duration-300'> 
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
+                <span className='group-hover:rotate-90 duration-300'> 
                     <HiArrowNarrowRight className='ml-3' /> 
                 </span>
                 </button>
