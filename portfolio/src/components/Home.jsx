7dda65e20ae509884dc1c93335f5bd933db29d17
singhible <bbnpreetsingh@gmail.com>
2022-11-06 13:27:19 -0800

    Added the bio and the buttons for view-project

--- portfolio/src/components/Home.jsx
+++ portfolio/src/components/Home.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 import {HiArrowNarrowRight} from 'react-icons/hi';
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'> 
-
+    <div name='home' className='w-full h-screen bg-[#0a192f]'> 
+        {/*container */}
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hi, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Babanpreet Singh</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm an aspiring Full Stack developer.</h2>
+            <p className='text-[#8892b0] py-4 max-w-[700px]'>I'm also an university student who is currently building its web-based MacOS. </p>
+            <div>
+                <button className='text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Projects
+                <span className='hover:rotate-90 duration-300'> 
+                    <HiArrowNarrowRight className='ml-3' /> 
+                </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
