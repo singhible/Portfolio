d9f6357d747f129a434ec195966aa972209a6c1c
singhible <bbnpreetsingh@gmail.com>
2022-11-06 12:01:40 -0800

    styled the navbar and added responsiveness using media query. Now starting to add states for the mobile menu and the default web browser

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,30 +1,38 @@
-import React from 'react'
-import {Fabars, FaTimes} from 'react-icons/fa';
+import React, {useState} from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa';
 import Logo from '../assests/logo.png';
 const Navbar = () => {
+    const [nav,setNav] = useState(false);
+    const handleclick = () => setNav(!nav);
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
         <div>
             <img src={Logo} alt= "Logo" style={{width: '50px'}} />
         </div>
         {/* menu*/ }
-        <div>
-            <ul>
+            <ul className='hidden md:flex'>
                 <li>Home</li>
                 <li>About</li>
                 <li>SKills</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
-        </div>
         {/** Hamburger menu*/}
-        <div></div>
+        <div className='md:hidden z-10'>
+            <FaBars />
+        </div>
 
         {/*Mobile menu */}
-        <div></div>
+           <ul className='hidden absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'>
+                <li className='py-6' text-4xl>Home</li>
+                <li className='py-6' text-4xl>About</li>  
+                <li className='py-6' text-4xl>SKills</li>
+                <li className='py-6' text-4xl>Projects</li>
+                <li className='py-6' text-4xl>Contact</li>
+            </ul>
 
         {/*Social icons*/}
-        <div></div>
+        <div className='hidden'></div>
 
     </div>
   )
